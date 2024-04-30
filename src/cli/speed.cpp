6566e0e83e07f0c7ae8dcc25c2794d0a3d38a2bd
Jack Lloyd <jack@randombit.net>
2024-04-30 18:28:13 -0400

    Split pcurves to its own cmdlet in speed

--- src/cli/speed.cpp
+++ src/cli/speed.cpp
@@ -547,6 +547,8 @@ class Speed final : public Command {
 #if defined(BOTAN_HAS_PCURVES)
             else if(algo == "ECDSA-pcurves") {
                bench_pcurve_ecdsa(ecc_groups, msec);
+            } else if(algo == "pcurves") {
+               bench_pcurves(ecc_groups, msec);
             }
 #endif
 
@@ -1134,9 +1136,6 @@ class Speed final : public Command {
             auto mult_timer = make_timer(group_name + " Montgomery ladder");
             auto blinded_mult_timer = make_timer(group_name + " blinded comb");
             auto blinded_var_mult_timer = make_timer(group_name + " blinded window");
-            auto pcurves_base_timer = make_timer(group_name + " pcurve base");
-            auto pcurves_var_timer = make_timer(group_name + " pcurve var");
-            auto pcurves_mul2_timer = make_timer(group_name + " pcurve mul2");
 
             const Botan::EC_Point& base_point = ec_group.get_base_point();
 
@@ -1162,35 +1161,11 @@ class Speed final : public Command {
 
                BOTAN_ASSERT_EQUAL(r1, r2, "Same point computed by Montgomery and comb");
                BOTAN_ASSERT_EQUAL(r1, r3, "Same point computed by Montgomery and window");
-
-   #if defined(BOTAN_HAS_PCURVES)
-               if(auto curve = Botan::PCurve::PrimeOrderCurve::from_name(group_name)) {
-                  const auto scalar_bytes = Botan::BigInt::encode_1363(scalar, ec_group.get_order_bytes());
-
-                  if(auto s = curve->deserialize_scalar(scalar_bytes)) {
-                     pcurves_base_timer->run([&]() { return curve->mul_by_g(s.value(), rng()).to_affine(); });
-
-                     auto g = curve->generator();
-                     auto g_tab = curve->mul_setup(g);
-                     pcurves_var_timer->run(
-                        [&]() { return curve->mul_with_table(*g_tab, s.value(), rng()).to_affine(); });
-
-                     auto h = curve->mul(g, s.value(), rng()).to_affine();
-                     pcurves_mul2_timer->run(
-                        [&]() { return curve->mul2_vartime(g, s.value(), h, s.value()).to_affine(); });
-                  }
-               }
-   #endif
             }
 
             record_result(mult_timer);
             record_result(blinded_mult_timer);
             record_result(blinded_var_mult_timer);
-            if(pcurves_base_timer->events() > 0) {
-               record_result(pcurves_base_timer);
-               record_result(pcurves_var_timer);
-               record_result(pcurves_mul2_timer);
-            }
          }
       }
 
@@ -1249,6 +1224,45 @@ class Speed final : public Command {
 #endif
 
 #if defined(BOTAN_HAS_PCURVES)
+
+      void bench_pcurves(const std::vector<std::string>& groups, const std::chrono::milliseconds runtime) {
+         for(const auto& group_name : groups) {
+            if(auto curve = Botan::PCurve::PrimeOrderCurve::from_name(group_name)) {
+               auto pcurves_base_timer = make_timer(group_name + " pcurve base mul");
+               auto pcurves_var_timer = make_timer(group_name + " pcurve var mul");
+               auto pcurves_mul2_timer = make_timer(group_name + " pcurve mul2");
+
+               auto scalar_invert = make_timer(group_name + " pcurve scalar invert");
+               auto to_affine = make_timer(group_name + " pcurve proj->affine");
+
+               const auto scalar = curve->random_scalar(rng());
+
+               pcurves_base_timer->run_until_elapsed(runtime,
+                                                     [&]() { return curve->mul_by_g(scalar, rng()).to_affine(); });
+
+               auto g = curve->generator();
+               auto g_tab = curve->mul_setup(g);
+               pcurves_var_timer->run_until_elapsed(
+                  runtime, [&]() { return curve->mul_with_table(*g_tab, scalar, rng()).to_affine(); });
+
+               auto h = curve->mul(g, curve->random_scalar(rng()), rng()).to_affine();
+               const auto scalar2 = curve->random_scalar(rng());
+               pcurves_mul2_timer->run_until_elapsed(
+                  runtime, [&]() { return curve->mul2_vartime(g, scalar, h, scalar2).to_affine(); });
+
+               auto pt = curve->mul(g, curve->random_scalar(rng()), rng());
+               to_affine->run_until_elapsed(runtime, [&]() { pt.to_affine(); });
+
+               scalar_invert->run_until_elapsed(runtime, [&]() { scalar.invert(); });
+
+               record_result(pcurves_base_timer);
+               record_result(pcurves_var_timer);
+               record_result(pcurves_mul2_timer);
+               record_result(to_affine);
+               record_result(scalar_invert);
+            }
+         }
+      }
 
       void bench_pcurve_ecdsa(const std::vector<std::string>& groups, const std::chrono::milliseconds runtime) {
          for(const auto& group_name : groups) {
